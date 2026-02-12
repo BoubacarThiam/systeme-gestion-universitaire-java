953525e78253407bb5250e1c573a1cce6a38c0c2
Boubacar Thiam <boubacarthiam005@icloud.com>
2026-02-12 18:58:28 +0000

    new code

---
+++ src/main/MainApp.java
@@ -0,0 +1,47 @@
+package main;
+
+import service.EtudiantService;
+import service.MatiereService;
+import service.NoteService;
+import util.GestionFichier;
+import util.Menu;
+
+/**
+ * Classe principale du Système de Gestion Universitaire
+ * Point d'entrée de l'application
+ * @author Votre Nom
+ * @version 1.0
+ */
+public class MainApp {
+    
+    public static void main(String[] args) {
+        
+        // Initialisation des services
+        EtudiantService etudiantService = new EtudiantService();
+        MatiereService matiereService = new MatiereService();
+        NoteService noteService = new NoteService(etudiantService);
+        
+        // Chargement des données depuis les fichiers
+        GestionFichier.chargerTout(etudiantService, matiereService, noteService);
+        
+        // Initialiser des données de test si c'est la première utilisation
+        GestionFichier.initialiserDonneesTest(etudiantService, matiereService, noteService);
+        
+        // Création et lancement du menu
+        Menu menu = new Menu(etudiantService, matiereService, noteService);
+        
+        // Affichage de l'en-tête
+        menu.afficherEntete();
+        
+        // Message de bienvenue
+        System.out.println("\n Bienvenue dans le Système de Gestion Universitaire");
+        System.out.println("─".repeat(55));
+        System.out.println("Étudiants chargés : " + etudiantService.compterEtudiants());
+        System.out.println("Matières chargées : " + matiereService.compterMatieres());
+        System.out.println("Notes chargées : " + noteService.compterNotes());
+        System.out.println("─".repeat(55));
+        
+        // Lancement du menu principal
+        menu.afficherMenuPrincipal();
+    }
+}
